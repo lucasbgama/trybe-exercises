106cfb79e664e83b63984edfe73babd88b01cd8d
Lucas Gama <lucasbg@protonmail.com>
2020-10-26 19:21:58 -0300

    bubble sort

---
+++ Fundamentos_do_desenvolvimento_web/bloco_4/dia_2/exercicio10.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//bubble sort
+for(j=numbers.length - 1; j>0; j-=1){
+    for (let i=0; i< j; i+=1){
+        if(numbers[i]>numbers[i+1]){
+            let aux = numbers[i];
+            numbers[i] = numbers[i+1];
+            numbers[i+1] = aux;
+        }
+    }
+}
+
+console.log(numbers);
