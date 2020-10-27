81641568db1bc55f892bf28b5f0a47a81c09def1
Lucas Gama <lucasbg@protonmail.com>
2020-10-27 20:12:56 -0300

    Adiciona possibilidade para n par

--- Fundamentos_do_desenvolvimento_web/bloco_4/dia_3/exercicio04.js
+++ Fundamentos_do_desenvolvimento_web/bloco_4/dia_3/exercicio04.js
@@ -1,7 +1,13 @@
-let n = 5;
+let n = 7;
 
 let linhas = Math.ceil(n/2);
 
-for (let linha = 1; linha <= linhas; linha +=1){
-    console.log(' '.repeat(n-linha) + '*'.repeat(2*linha - 1));
+if(n%2 === 1){
+    for (let linha = 1; linha <= linhas; linha +=1){
+        console.log(' '.repeat(n-linha) + '*'.repeat(2*linha - 1));
+    }
+} else{
+    for (let linha = 1; linha <= linhas; linha +=1){
+        console.log(' '.repeat(n-linha) + '*'.repeat(2*linha));
+    }
 }
