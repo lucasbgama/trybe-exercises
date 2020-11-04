b7661418659f6d32404cdbc5e73583f5881e1bd2
Lucas Gama <lucasbg@protonmail.com>
2020-11-04 16:10:59 -0300

    Exercicio 1 - adiciona os dias no calendario e coloca classes para feriados e sextas feiras

--- Fundamentos_do_desenvolvimento_web/bloco_5/dia_3/script.js
+++ Fundamentos_do_desenvolvimento_web/bloco_5/dia_3/script.js
@@ -13,4 +13,21 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  let days = document.getElementById('days');
+
+  for(let day of dezDaysList){
+      let elementDay = document.createElement('li');
+      elementDay.className = 'day';
+      elementDay.innerHTML = day;
+      days.appendChild(elementDay);
+
+      if(day == 24 || day == 25 || day == 31){
+          elementDay.classList.add('holiday');
+      }
+
+      if(day == 4 || day == 11 || day == 18 || day == 25){
+        elementDay.classList.add('friday');
+      }
+  }
