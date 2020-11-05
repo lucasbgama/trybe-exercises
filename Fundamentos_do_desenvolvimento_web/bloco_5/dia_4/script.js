0dc4f89d2aa2dce002a1e951b8f6ed2216bef26c
Lucas Gama <lucasbg@protonmail.com>
2020-11-05 19:38:33 -0300

    Codigo refatorado para um loop com todas as configuracoes

--- Fundamentos_do_desenvolvimento_web/bloco_5/dia_4/script.js
+++ Fundamentos_do_desenvolvimento_web/bloco_5/dia_4/script.js
@@ -2,6 +2,8 @@ let config = document.getElementById('settings');
 let confButton = document.getElementById('settings-button');
 
 let confs = ['cor de fundo', 'cor do texto', 'tamanho da fonte', 'espacamento entre linhas', 'font family'];
+let confsJs = ['backgroundColor', 'color', 'fontSize', 'lineHeight','fontFamily'];
+
 function addConf(nome){
     let caixaTexto = document.createElement('input');
     caixaTexto.type = 'text';
@@ -26,19 +28,11 @@ confButton.addEventListener('click', function(){
     }
 });
 
-
-let corDeFundo = config.children[0];
-document.body.style.backgroundColor = localStorage.getItem('background-color');
-corDeFundo.addEventListener('keyup',function(e){
-    document.body.style.backgroundColor = e.target.value;
-    localStorage.setItem('background-color',e.target.value );
-})
-
-
-
-let corTexto = config.children[1];
-document.body.style.color = localStorage.getItem('color');
-corTexto.addEventListener('keyup',function(e){
-    document.body.style.color = e.target.value;
-    localStorage.setItem('color',e.target.value );
-})
+for(index = 0; index< config.children.length; index+=1){
+    let propertyJs = confsJs[index];
+    document.body.style[propertyJs] = localStorage.getItem(propertyJs);
+    config.children[index].addEventListener('keyup',function(e){
+        document.body.style[propertyJs]=e.target.value;
+        localStorage.setItem(propertyJs, e.target.value);
+    })
+}
